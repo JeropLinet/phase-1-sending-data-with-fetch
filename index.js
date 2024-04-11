d1af4ef09d20e3a4b9fbc0f2d4bd0f0acce05a65
Linet <teweilinet@gmail.com>
2024-04-11 12:07:08 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-// // Add your code here
-// function submitData(){
-//     let userName='Byron'
-//     let userEmail='byronn@gmail.com'
-
-//     const configurationObject={
-//         method:'POST',
-//         header:{
-//             "Content-Type": "application/json",
-//             "Accept": "application/json",
-//         },
-//         body:JSON.stringify({
-//               name:userName,
-//               email:userEmail,
-//         }),
-//     }
-
-//     fetch('http://localhost:3000/users',configurationObject)
-//     .then(res => res.json)
-//     .then(data => console.log(data))
-//     .catch(error => console.error('Yea I do not have that',error))
-// }
-
 function submitData(name, email) {
-    const url = 'http://localhost:3000/users';
+    const baseURL = 'http://localhost:3000/users';
     const headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     };
     const body = JSON.stringify({ name, email });
 
-    const requestOptions = {
+    const confugurationObject = {
         method: 'POST',
         headers:{
             "Content-Type": "application/json",
@@ -41,12 +18,12 @@ function submitData(name, email) {
         }),
     };
 
-    return fetch(url, requestOptions)
-        .then(response => {
-            if (!response.ok) {
+    return fetch(baseURL, confugurationObject)
+        .then(res => {
+            if (!res.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json();
+            return res.json();
         })
         .then(data => {
             const newId = data.id;
